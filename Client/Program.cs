750202962845108549ca4162ef119af12b839d36
Christian Giesswein <christian@software.tirol>
2021-06-16 14:46:07 +0200

    Initial

---
+++ Client/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static async Task Main()
+        {
+            var hub = new HubConnectionBuilder()
+                .WithUrl("https://localhost:5001/".TrimEnd('/') + "/MyHub")
+                .WithAutomaticReconnect()
+                .Build();
+            await hub.StartAsync();
+            hub.On<Msg>("Msg", message =>
+            {
+                Console.WriteLine("Received!");
+            });
+            Console.ReadLine();
+        }
+
+    }
+
+    internal class Msg
+    {
+        public byte[] Juhu { get; set; }
+    }
+}
---
+++ Server/Controllers/DemoController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace WebApplication3.Controllers
+{
+    public class DemoController : ControllerBase
+    {
+        public class MyHub : Hub
+        {
+        }
+        private readonly IHubContext<MyHub> hub;
+
+        public DemoController(IHubContext<MyHub> hub)
+        {
+            this.hub = hub;
+        }
+
+        [Route("Juhu")]
+        [HttpGet]
+        public async Task Start()
+        {
+            var r = new Random();
+            for (var i = 0; i < 100; i++)
+            {
+                var data = new byte[1024 * 1024];
+                r.NextBytes(data);
+                await hub.Clients.All.SendAsync("Msg", new
+                {
+                    Juhu = data
+                });
+                await Task.Delay(100);
+            }
+        }
+    }
+}
